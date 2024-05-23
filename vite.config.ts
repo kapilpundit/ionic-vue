8771f63cfcf7cfeafb4316228d87ce9ee4cb488f
Kapil Sharma <kapil.s@rglabs.net>
2024-05-23 17:44:06 +0530

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import legacy from '@vitejs/plugin-legacy'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000 // You can specify any port you prefer
+  },
+  plugins: [
+    vue(),
+    legacy()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  }
+})
